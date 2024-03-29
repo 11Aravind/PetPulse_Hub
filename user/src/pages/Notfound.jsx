1e0e1a8073dc2fd63699a43bc129c9d27053cbb7
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 13:57:49 +0530

    notfound component was created

---
+++ user/src/pages/Notfound.jsx
@@ -0,0 +1,10 @@
+import "./CSS/Notfound.css"
+const Notfound = () => {
+    return (
+        <div className="topSpacing">
+            <h1 className="notFoundHeadding">404</h1>
+            <h2 className="notFoundDesc">The page you are looking for not avaible!</h2>
+        </div>
+    );
+}
+export default Notfound;

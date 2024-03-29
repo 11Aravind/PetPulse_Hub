6d049ade9742c08019d4f161d1268c747b1b6aea
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 20:21:15 +0530

    some component was added

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    const menuData = [
+        {
+            menu: 'Dashboard',
+            url: "/",
+            subMenu: {
+                menu: 'menu 1',
+                url: "/"
+            },
+        },
+        {
+            menu: 'Service',
+            subMenu: [
+                {
+                    menu: 'Category',
+                    url: "/category"
+                },
+                {
+                    menu: 'Product',
+                    url: "/productdetails"
+                },
+                {
+                    menu: 'Caretaker',
+                    url: "/caretaking"
+                },
+                {
+                    menu: 'Order',
+                    url: "/orderdetails"
+                },
+                {
+                    menu: 'Blog',
+                    url: "/blogs"
+                },
+                {
+                    menu: 'Gallery',
+                    url: "/gallery"
+                },
+            ],
+        },
+    ];
+    const[navbarShowOrHide,isToogleVisibility]=useState(false);
+    const navbarCallBack=(e)=>{
+        isToogleVisibility(!navbarShowOrHide);
+        console.log(navbarShowOrHide);
+    }
+    return (
+        <>
+            <Topnavbar  navbarShowOrHide={navbarShowOrHide} navbarCallBack={navbarCallBack}/>
+            
+            <div className={navbarShowOrHide?"content-container show":"hide"}>
+                <div className="side-nav-container" >
+                    {menuData.map((menuItem, index) => (
+                        <div className="menu-container" key={index}>
+                            <div className="main-menu-headding">{menuItem.menu}</div>
+
+                            {Array.isArray(menuItem.subMenu) ? (
+                                <>
+                                    {menuItem.subMenu.map((subMenuItem, subIndex) => (
+                                        <Link to={subMenuItem.url} key={subIndex}>
+                                            <div className="submenu" key={subIndex}>{subMenuItem.menu}</div>
+
+                                        </Link>))}
+                                </>
+                            ) : (
+                                <Link to={menuItem.subMenu.url}>
+                                    <div className="submenu" >{menuItem.subMenu.menu}</div>
+                                </Link>
+                                
+                            )}
+                        </div>
+
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
+export default Navbar;
+export const Topnavbar = ({navbarShowOrHide,navbarCallBack}) => {
+   
+    return (
+        <div className="topnav-container">
+           
+            <div className="top-leftContainer">
+            <div className="logo">
+            <img src="https://img.freepik.com/premium-vector/dog-paw-animal-paws_77417-1636.jpg?w=740" alt="logo" style={{width:"40px"}}/>
+            </div>
+            <div className="closingBtn" onClick={navbarCallBack}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+            </div>
+            <div className="admin-name">
+            {navbarShowOrHide}
+                Admin
+            </div>
+        </div>
+    );
+}

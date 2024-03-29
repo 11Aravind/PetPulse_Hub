36464b32c5b353eb72207e2e4a07959e31597ba0
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 13:50:51 +0530

    category slider was added

---
+++ user/src/component/Categoryslider.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './CSS/categoryslider.css'; // Import CSS file
+
+const Categoryslider = ({categorys}) => {
+  const containerRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const updateScrollPosition = () => {
+      if (containerRef.current) {
+        setScrollPosition(containerRef.current.scrollLeft);
+      }
+    };
+
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollPosition);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", updateScrollPosition);
+      }
+    };
+  }, []);
+
+  const scrollLeft = () => {
+    const newPosition = scrollPosition - containerRef.current.offsetWidth / 2;
+    setScrollPosition(newPosition >= 0 ? newPosition : 0);
+    containerRef.current.scrollTo({
+      left: newPosition,
+      behavior: 'smooth'
+    });
+  };
+
+  const scrollRight = () => {
+    const newPosition = scrollPosition + containerRef.current.offsetWidth / 2;
+    const maxScroll = containerRef.current.scrollWidth - containerRef.current.clientWidth;
+    setScrollPosition(newPosition <= maxScroll ? newPosition : maxScroll);
+    containerRef.current.scrollTo({
+      left: newPosition,
+      behavior: 'smooth'
+    });
+  };
+  // const containerRef = useRef(null);
+  // const [scrollPosition, setScrollPosition] = useState(0);
+
+  // useEffect(() => {
+  //   const updateScrollPosition = () => {
+  //     if (containerRef.current) {
+  //       setScrollPosition(containerRef.current.scrollLeft);
+  //     }
+  //   };
+
+  //   containerRef.current.addEventListener("scroll", updateScrollPosition);
+  //   return () => {
+  //     containerRef.current.removeEventListener("scroll", updateScrollPosition);
+  //   };
+  // }, []);
+
+  // const scrollLeft = () => {
+  //   const newPosition = scrollPosition - containerRef.current.offsetWidth / 2;
+  //   setScrollPosition(newPosition >= 0 ? newPosition : 0);
+  //   containerRef.current.scrollTo({
+  //     left: newPosition,
+  //     behavior: 'smooth'
+  //   });
+  // };
+
+  // const scrollRight = () => {
+  //   const newPosition = scrollPosition + containerRef.current.offsetWidth / 2;
+  //   const maxScroll = containerRef.current.scrollWidth - containerRef.current.clientWidth;
+  //   setScrollPosition(newPosition <= maxScroll ? newPosition : maxScroll);
+  //   containerRef.current.scrollTo({
+  //     left: newPosition,
+  //     behavior: 'smooth'
+  //   });
+  // };
+
+
+  return (
+    <div className="container">
+      <div className="main-slider">
+      <h1 className='headding'>Main Categorys</h1>
+        <div className="label-container">
+          <button className="arrow-left" onClick={scrollLeft}>
+            <i className="bi bi-arrow-left"></i>
+          </button>
+          <button className="arrow-right" onClick={scrollRight}>
+            <i className="bi bi-arrow-right"></i>
+          </button>
+        </div>
+        <div className="card-slider-main" ref={containerRef}>
+          {
+          
+          categorys.map((category,index) => (
+            <div className="card-wrapper" key={index}>
+              {/* <img
+                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png`}
+                alt=""
+              /> */}
+              <img src={`http://localhost:5000/${category.image}`} alt="" />
+              <span>{category.mainCategory}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categoryslider;

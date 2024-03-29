f5a00302eb0b4dec9dbe4f491aa494e5c6a1c7d3
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 13:56:27 +0530

    home component was created

---
+++ user/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import ImageSlider from "../component/ImageSlider";
+import {Cardslider} from "../component/ImageSlider";
+// import Categorylist from "../component/Categorylist";
+// import MyCarousel  from "../component/CarouselComponent";
+import Categoryslider from "../component/Categoryslider";
+import {  useSelector } from "react-redux";
+
+const Home = () => {
+    const categorys=useSelector((state)=>state.categorys.categoryList);
+    console.log(categorys);
+    return (
+      <div className="container">
+        <ImageSlider />
+            {/* 
+            <h1 className="headding">Our Categorys</h1>
+        <Categorylist/> */}
+            <Categoryslider categorys={categorys}/>
+            <h1 className="headding">Pet-tastic Deals</h1>
+            {/* <MyCarousel  items={newProducts}/> */}
+            <Cardslider />
+      </div>
+    );
+}
+export default Home;

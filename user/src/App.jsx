c2950531ed8166119401f6c5fb345a93faea2996
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 13:41:55 +0530

    app component was edited

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import AllRouter from "./AllRoutes";
+import Navbar from "./component/Navbar";
+import { useDispatch } from "react-redux"
+import { fetchAndStore } from "./Slice/productSlice"
+import { useEffect } from "react";
+import {httpRequest} from "./API/api"
+import {fetchAndStoreCategory} from "./Slice/categorySlice"
+// import Toplinks from "./component/Toplinks";
+//  import Footer from "./component/Footer"
+const App = () => {
+      const dispatch=useDispatch();
+  useEffect(() => {
+    // Fetching categories
+    httpRequest('get', 'api/category')
+    .then(data => {
+      if (data && Array.isArray(data.categoryDetails)) {
+        dispatch(fetchAndStoreCategory(data.categoryDetails));
+      } else {
+        console.error("Fetched data does not contain 'categoryDetails' array:", data);
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching products:", error);
+    });
+  
+    // Fetching products
+    httpRequest('get', 'api/product')
+      .then(data => {
+        if (data && Array.isArray(data.productDetails)) {
+          dispatch(fetchAndStore(data.productDetails));
+        } else {
+          console.error("Fetched data does not contain 'productDetails' array:", data);
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Toplinks /> */}
+      <Navbar />
+      <AllRouter />
+      {/* <Footer/> */}
     </>
-  )
+  );
 }
-
-export default App
+export default App;

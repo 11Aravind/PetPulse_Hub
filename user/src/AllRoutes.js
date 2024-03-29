e085562d63b6d846e8a97fbf52a097baa540af45
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-29 14:01:54 +0530

    all routes was added

---
+++ user/src/AllRoutes.js
@@ -0,0 +1,67 @@
+import Home from "./pages/Home";
+import Pets from "./pages/Pets";
+import Foods from "./pages/Foods";
+import Accessorys from "./pages/Accessorys";
+import {Login} from "./pages/Login";
+import {Signup} from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
+import Notfound from "./pages/Notfound";
+import Productdetails from "./pages/Productdetails";
+import Cart from "./pages/Cart";
+const routerInfo = [
+    {
+        path: "/",
+        component: <Home />,
+    },
+    {
+        path: "/pets",
+        component: <Pets />
+    },
+    {
+        path: "/foods",
+        component: <Foods />
+    },
+    {
+        path: "/accessorys",
+        component: <Accessorys />
+    },
+    {
+        path: "/productdetails/:id",
+        component: <Productdetails />
+    },
+    {
+        path: "/login",
+        component: <Login />
+    },
+    {
+        path: "/signup",
+        component: <Signup />
+    },
+    {
+        path: "/cart",
+        component: <Cart />
+    },
+    {
+        path: "*",
+        component: <Notfound />
+    }
+];
+const AllRouter = () => {
+    return (
+        <Routes>
+            {
+                routerInfo.map((eachRoute, id) => {
+                    return (
+                        <Route
+                            key={id}
+                            path={eachRoute.path}
+                            element={eachRoute.component}
+                        />
+                    );
+                })
+            }
+
+        </Routes>
+    );
+}
+export default AllRouter;

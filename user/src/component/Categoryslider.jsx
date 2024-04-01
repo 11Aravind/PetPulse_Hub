763d35417569ff44935f03f0421065a550b67f6d
11Aravind <aravindsreenandanam11@gmail.com>
2024-04-01 16:46:53 +0530

    product filter and displaying was  added

--- user/src/component/Categoryslider.jsx
+++ user/src/component/Categoryslider.jsx
@@ -58,7 +58,7 @@ const Categoryslider = ({ categorys,headding }) => {
           {
 
             categorys.map((category, index) => (
-              <div className="card-wrapper" key={index}>
+              <div className="card-wrapper" key={index} id={category._id}>
                 {/* <img
                 src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png`}
                 alt=""
--- user/src/pages/Foods.jsx
+++ user/src/pages/Foods.jsx
@@ -3,7 +3,8 @@ import Produtcard from  "../component/Produtcard";
 const Foods=()=>{
   const categorys=useSelector((state)=>state.categorys.categoryList);
     const products = useSelector((state) => state.products.productList);
-    const filterCategory=categorys.filter((itemCat)=>itemCat.mainCategory==="Food" && products.some((productItem)=>productItem.category_id===itemCat._id ));
+    // && products.some((productItem)=>productItem.category_id===itemCat._id )
+    const filterCategory=categorys.filter((itemCat)=>itemCat.mainCategory==="Food" );
     const filteredProduct=products.filter(product=>categorys.some(category=>category._id===product.category_id && category.mainCategory==="Food"));
     // console.log(filteredProduct);
     return(

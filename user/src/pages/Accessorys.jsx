40a08cf59876722402c0e780af6cfe9849a9cf02
11Aravind <aravindsreenandanam11@gmail.com>
2024-04-01 09:18:35 +0530

    accessorys component was updated

--- user/src/pages/Accessorys.jsx
+++ user/src/pages/Accessorys.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+import Produtcard from "../component/Produtcard"
 const Accessorys=()=>{
+    const categorys=useSelector((state)=>state.categorys.categoryList);
+    const products = useSelector((state) => state.products.productList);
+    const filterCategory=categorys.filter((itemCat)=>itemCat.mainCategory==="Accessorys" && products.some((productItem)=>productItem.category_id===itemCat._id ));
+    const filteredProduct=products.filter(product=>categorys.some(category=>category._id===product.category_id && category.mainCategory==="Accessorys"));
     return(
-        <div className="topSpacing">Accessorys component</div>
+        <Produtcard categorys={filterCategory} products={filteredProduct}/>
     );
 }
 export default Accessorys;

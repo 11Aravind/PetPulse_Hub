96c8d3ea13b345a8d76d347b14863d9d1ca19f63
11Aravind <aravindsreenandanam11@gmail.com>
2024-04-01 09:19:41 +0530

    food and pets component was edited

--- user/src/pages/Foods.jsx
+++ user/src/pages/Foods.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
+import Produtcard from  "../component/Produtcard";
 const Foods=()=>{
-
+  const categorys=useSelector((state)=>state.categorys.categoryList);
+    const products = useSelector((state) => state.products.productList);
+    const filterCategory=categorys.filter((itemCat)=>itemCat.mainCategory==="Food" && products.some((productItem)=>productItem.category_id===itemCat._id ));
+    const filteredProduct=products.filter(product=>categorys.some(category=>category._id===product.category_id && category.mainCategory==="Food"));
+    // console.log(filteredProduct);
     return(
-        <div className="topSpacing">
-        <h1>foood</h1>
-     </div>
+      <Produtcard categorys={filterCategory} products={filteredProduct} />
    )
 }
 export default Foods;
--- user/src/pages/Pets.jsx
+++ user/src/pages/Pets.jsx
@@ -1,44 +1,15 @@
-import { Link } from "react-router-dom"
+
 import { useSelector } from "react-redux"
 import Categoryslider from "../component/Categoryslider";
+import Produtcard from "../component/Produtcard";
 import "../component/CSS/Card.css"
 const Pets = () => {
+    const categorys=useSelector((state)=>state.categorys.categoryList.filter(item=>item.mainCategory=="Pets"));
     const products = useSelector((state) => state.products.productList);
-    const categorys=useSelector((state)=>state.categorys.categoryList);
+    const filteredProduct=products.filter(product=>categorys.some(category=>category._id===product.category_id && category.mainCategory==="Pets"));
+    // console.log(products);
     return (
-        <div className="topSpacing">
-            {/* <h1 className="headding">Pets</h1> */}
-            <Categoryslider categorys={categorys} headding={"Our Pets"}/>
-            <h1 className="headding">New Products</h1>
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-4">
-                {
-                    products.map((product, index) => {
-                        return (
-                            <div className="col-6 col-sm-6 col-md-4 col-lg-3" key={index}>
-                                <Link to={`/productdetails/${product._id}`} className="card-link">
-                                    <div className="card h-100">
-                                        <div className="img-container">
-                                            <img src={`http://localhost:5001/${product.image}`} className="card-img-top product-image" alt="..." />
-                                            <button className="options-btn">view option</button>
-                                        </div>
-                                        <div className="card-body">
-                                            <div className="card-title">{product.name}</div>
-                                            <div className="cart-footer">
-                                                <div className="price">
-                                                    <div className="oldPrice">₹{product.oldPrice}</div>
-                                                    <div className="newprice">₹{product.newPrice}</div>
-                                                </div>
-                                                <div><button className="cardBtn">Add to Cart</button></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
+       <Produtcard categorys={categorys} products={filteredProduct}/>
     )
 }
 export default Pets;

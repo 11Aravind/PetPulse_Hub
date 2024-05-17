174a6fb3b90a092e5c4fc9910320004f4cd3e828
11Aravind <aravindsreenandanam11@gmail.com>
2024-05-17 08:52:10 +0530

    address model was created

---
+++ Backend/models/Address.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema
+const addressSchema = new schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref:"User",
+        require: true
+    },
+    name: {
+        type:String,
+        require:true
+    },
+    mobileNo: {
+        type:Number,
+        require:true,
+        maxlength:10
+    },
+    pincode: {
+        type:Number,
+        require:true
+    },
+    locality: {
+        type:String,
+        require:true
+    },
+    address: {
+        type:String,
+        require:true
+    },
+    city: {
+        type:String,
+        require:true
+    },
+    state: {
+        type:String,
+        require:true
+    },
+    landmark: {
+        type:String,
+        require:true
+    },
+    address_type: {
+        type:String,
+        require:true
+    }
+})
+export default mongoose.model("Address",addressSchema);

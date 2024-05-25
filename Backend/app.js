69e5b58c839200c8906095d4925f8d0abf6058ed
11Aravind <aravindsreenandanam11@gmail.com>
2024-05-25 18:13:30 +0530

    razorpay payment gatway some code was added

--- Backend/app.js
+++ Backend/app.js
@@ -7,10 +7,19 @@ import galleryRouter from "./routes/gallery-routs.js";
 import categoryRouter from "./routes/category-routers.js";
 import adminRouter from "./routes/admin-router.js";
 import productRouter from "./routes/product-routers.js";
+import Razorpay from "razorpay"
+import crypto from "crypto"
 import cors from "cors";
+// const Razorpay = require("razorpay");
+// const crypto = require("crypto");
+// require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.urlencoded({ extended: false }));
 
-const app=express();
 // Middleware to set Permissions-Policy header
+const PORT = process.env.PORT;
 app.use((req, res, next) => {
   res.setHeader(
     "Permissions-Policy",
@@ -21,17 +30,53 @@ app.use((req, res, next) => {
 // app.use(express.static('uploads'));
 app.use(express.json())
 app.use(cors())
-const PORT=5001
 // Serve images from a directory named 'images'
 // app.use('/', express.static('uploads'));
 
 app.use(express.static('uploads'));
-app.use("/api/user",userRouter); 
-app.use("/api/blog",blogRouter); 
-app.use("/api/gallery",galleryRouter); 
-app.use("/api/category",categoryRouter); 
-app.use("/api/product",productRouter); 
-app.use("/api/admin",adminRouter);
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/gallery", galleryRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api/admin", adminRouter);
+app.post("/order", async (req, res) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    const options = req.body;
+    const order = await razorpay.orders.create(options);
+
+    if (!order) {
+      return res.status(500).send("Error");
+    }
+
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.post("/order/validate", async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+  const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+  //order_id + "|" + razorpay_payment_id
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+  const digest = sha.digest("hex");
+  if (digest !== razorpay_signature) {
+    return res.status(400).json({ msg: "Transaction is not legit!" });
+  }
+  res.json({
+    msg: "success",
+    orderId: razorpay_order_id,
+    paymentId: razorpay_payment_id,
+  });
+});
+
 mongoose.connect('mongodb://localhost:27017/petsworlds', {
   useNewUrlParser: true,
   useUnifiedTopology: true
--- user/src/pages/OrderConfirmation.jsx
+++ user/src/pages/OrderConfirmation.jsx
@@ -9,6 +9,79 @@ import "./CSS/OrderConfirmation.css";
 export const OrderConfirmation = () => {
   const { isEmpty, items, cartTotal } = useCart();
   console.log(items);
+
+
+  const amount = 1000000;
+  const currency = "INR";
+  const receiptId = "qwsaq1";
+
+  const paymentHandler = async (e) => {
+    const response = await fetch("http://localhost:5001/order", {
+      method: "POST",
+      body: JSON.stringify({
+        amount,
+        currency,
+        receipt: receiptId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const order = await response.json();
+    console.log(order);
+
+    var options = {
+      key: "rzp_test_u5nxL1KN1AKLE0", // Enter the Key ID generated from the Dashboard
+      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency,
+      name: "Petpulz hub", //your business name
+      description: "Test Transaction",
+      image: "./tshirt.jpg",
+      order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: async function (response) {
+        const body = {
+          ...response,
+        };
+
+        const validateRes = await fetch(
+          "http://localhost:5001/order/validate",
+          {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const jsonRes = await validateRes.json();
+        console.log(jsonRes);
+      },
+      prefill: {
+        //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+        name: "Web Dev Matrix", //your customer's name
+        email: "webdevmatrix@example.com",
+        contact: "9000000099", //Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#EE6043",
+      },
+    };
+    var rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+    rzp1.open();
+    e.preventDefault();
+  };
   //   const loginCredentials = JSON.parse(localStorage.getItem("loginCredentials"));
   //   const user_id = loginCredentials.user_id;
   // const [paymentMode, setPaymentMode] = useState("cod");
@@ -140,11 +213,15 @@ export const OrderConfirmation = () => {
           {/* <button className="onlinePayment">Online Payment</button> */}
         </div>
       </div>
-      <button className="checkOutBtn">Confirm</button>
+      <button className="checkOutBtn" onClick={paymentHandler}>Confirm</button>
     </div>
 
   );
 };
+
+
+
+
 const Address = ({ changeAddressVisibility }) => {
   const name = useRef("")
   const mobile = useRef("")

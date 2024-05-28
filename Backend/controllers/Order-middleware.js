e66be9d72686d0dece373dbda6bef361bb43a812
11Aravind <aravindsreenandanam11@gmail.com>
2024-05-28 20:15:29 +0530

    order middleware was added

---
+++ Backend/controllers/Order-middleware.js
@@ -0,0 +1,26 @@
+import Order from "../models/Order.js"
+import mongoose from "mongoose";
+export const orderStore = async(req, res) => {
+    // let checkoutDetails=
+    let a= new Date();
+    let date=a.getDate()+"/"+a.getMonth()+"/"+a.getFullYear()+" "+a.getHours()+":"+a.getMinutes()+":"+a.getSeconds()
+
+    const { userId, addressId, items, totelamount, transactionId, dateOfOrder, status, paymentMode, order_message } = req.body;
+    const checkOutDetails = new Order({
+        userId,
+        addressId,
+        items,
+        totelamount,
+        transactionId,
+        dateOfOrder:date,
+        status,
+        paymentMode,
+        order_message
+    })
+    try {
+        await checkOutDetails.save()
+    } catch (err) {
+       return res.status(404).json({status:false,message:`something went wrong ${err}`})
+    }
+    return res.status(200).json({status:true,message:"successfult store"})
+}

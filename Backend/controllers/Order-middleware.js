80eb4de7357a9fbf26a8d3a594ea55366deb3984
11Aravind <aravindsreenandanam11@gmail.com>
2024-05-30 20:35:45 +0530

    order middleware and router code was edited

--- Backend/controllers/Order-middleware.js
+++ Backend/controllers/Order-middleware.js
@@ -1,26 +1,84 @@
-import Order from "../models/Order.js"
 import mongoose from "mongoose";
-export const orderStore = async(req, res) => {
-    // let checkoutDetails=
-    let a= new Date();
-    let date=a.getDate()+"/"+a.getMonth()+"/"+a.getFullYear()+" "+a.getHours()+":"+a.getMinutes()+":"+a.getSeconds()
-
-    const { userId, addressId, items, totelamount, transactionId, status, paymentMode, order_message } = req.body;
-    const checkOutDetails = new Order({
-        userId,
-        addressId,
-        items,
-        totelamount,
-        transactionId,
-        dateOfOrder:date,
-        status,
-        paymentMode,
-        order_message
-    })
+import Order from "../models/Order.js"
+import Razorpay from "razorpay";
+import crypto from "crypto"
+export const storeOrder = async (req, res) => {
     try {
-       const savedDocument= await checkOutDetails.save();
-       return res.status(200).json({status:true,message:"successfult store",orderId:savedDocument._id});
+        const razorpay = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID,
+            key_secret: process.env.RAZORPAY_SECRET,
+        });
+        // Extract necessary information from the request body
+        const { userId, addressId, items, razorpayOrderId, status, paymentMode, order_message, amount, currency, receiptId } = req.body;
+
+        // Additional information to include in the order creation process
+        const options = {
+            amount,
+            currency,
+            receipt: receiptId,
+            notes: {
+                userId,
+                addressId,
+            },
+        };
+
+        // Create the order using Razorpay SDK
+        const order = await razorpay.orders.create(options);
+
+        if (!order) {
+            return res.status(500).send({ message: "rezorpay not create a order" });
+        }
+
+        // Save the order details to the database
+
+        let a = new Date();
+        let date = a.getDate() + "/" + a.getMonth() + "/" + a.getFullYear() + " " + a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds()
+        const newOrder = new Order({
+            userId,
+            addressId,
+            items,
+            totelamount: amount,
+            razorpayOrderId: order.id,
+            dateOfOrder: date,
+            status,
+            paymentMode,
+            order_message
+        })
+        try {
+            await newOrder.save();
+            // Return the order details
+            res.json(order);
+        } catch (err) {
+            console.log(err);
+
+            res.status(500).send({ message: "data not stored in db", error: err });
+        }
     } catch (err) {
-       return res.status(404).json({status:false,message:`something went wrong ${err}`,orderId:null});
+        console.log(err);
+        res.status(500).send({ message: "something went wrong", error: err });
     }
+}
+export const validatePaymentStatus = async (req, res) => {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+    //order_id + "|" + razorpay_payment_id
+    sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+    const digest = sha.digest("hex");
+    if (digest !== razorpay_signature) {
+        return res.status(400).json({ msg: "Transaction is not legit!" });
+    }
+    try {
+        const updateDocument = await Order.findOneAndUpdate(
+            { razorpayOrderId: razorpay_order_id },// condition,
+            { $set: { status: "success" } },
+            { new: true }
+        )
+    } catch (err) {
+        console.log(err);
+    }
+    res.json({
+        msg: "success",
+        orderId: razorpay_order_id,
+        paymentId: razorpay_payment_id,
+    });
 }

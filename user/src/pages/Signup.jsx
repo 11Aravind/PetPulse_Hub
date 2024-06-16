c1ee3d974f7ed5b7beba283df7676d05c9bf085c
11Aravind <aravindsreenandanam11@gmail.com>
2024-06-16 12:21:35 +0530

    signup form validation was added

--- user/src/pages/Signup.jsx
+++ user/src/pages/Signup.jsx
@@ -1,95 +1,121 @@
-
 import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
-import { httpRequest } from "../API/api"
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux"
-import {setRoute} from "../Slice/commonSlice"
+import { setRoute } from "../Slice/commonSlice"
+import { httpRequest } from "../API/api"
 import "./CSS/Login.css"
 
 export const Signup = () => {
+  const userId = JSON.parse(localStorage.getItem("userId"));
+  const [passwordLength, setErrorMsg] = useState(" ")
+  const [isValid, setIsValid] = useState(false);
+  const [samePassword, setMsg] = useState(" ")
   const [message, setMessage] = useState("");
   const email = useRef("");
   const password = useRef("");
   const confirmPassword = useRef("");
   const navigate = useNavigate();
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
+
   const showHideMessage = (msg) => {
     setMessage(msg);
     setTimeout(() => {
       setMessage("");
     }, 3000);
   }
-  const signUp = () => {
-    if (password.current.value != confirmPassword.current.value) {
-      showHideMessage("Enter same passsword");
+  const checkPasswordLength = (e) => {
+    const currentValue = e.target.value;
+    if (currentValue.length < 6) {
+      setErrorMsg("Password length must be >=6 ");
+    }
+    else if (confirmPassword.current.value !== "") {
+      checkPasswordMatch()
     }
     else {
-      const signupData = {
-        "email": email.current.value,
-        "password": password.current.value
-      }
-      httpRequest('post', 'api/user/signup', signupData)
-        .then((res) => {
-          showHideMessage(res.message);
-          dispatch(setRoute("/signup"))
-          navigate("/login")
-        })
-        .catch((err) => console.log(err));
-      // console.log(signupData);
-
+      setErrorMsg("")
     }
-
+  }
+  const checkPasswordMatch = () => {
+    if (password.current.value != confirmPassword.current.value) {
+      setMsg("Enter same passsword")
+    }
+    else
+      setMsg("")
+  }
+  const validateEmail = (e) => {
+    const email = e.target.value
+    // Regular expression pattern for a valid email address
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsValid(pattern.test(email) ? "" : "Enter valid e-mail");
+  };
+  const signUp = () => {
+    const signupData = {
+      "email": email.current.value,
+      "password": password.current.value
+    }
+    httpRequest('post', 'api/user/signup', signupData)
+      .then((res) => {
+        dispatch(setRoute("/signup"))
+        navigate("/login")
+      })
+      .catch((err) =>  {
+        showHideMessage("Something went wrong try again")
+      console.log(err);
+      });
   }
 
 
   return (
     <div className="login-container">
-      <div className="mx-auto col-10 col-md-8 col-lg-4 loginBox">
+      {userId === null ? <div className="mx-auto col-10 col-md-8 col-lg-4 loginBox">
         <h3 className="main-headding">Signup</h3>
         <p className="login-desc">Signup and join the pet community </p>
         <div className="errorMsg">{message}</div>
         <div className="form-group  txtBox-spacing">
-          {/* <label for="username">E-mail</label> */}
           <input
             type="text"
             className="form-control username"
             id="username"
             ref={email}
             placeholder="Email"
+            onChange={validateEmail}
           />
+          <small className="errorMsg">{isValid}</small>
         </div>
         <div className="form-group  txtBox-spacing">
-          {/* <label for="password">Password</label> */}
           <input
             type="password"
             className="form-control password"
             id="password"
             ref={password}
+            onChange={e => checkPasswordLength(e)}
             placeholder="Password"
           />
+          <small className="errorMsg">{passwordLength}</small>
         </div>
         <div className="form-group txtBox-spacing">
-          {/* <label for="cpassword">Confirm Password</label> */}
           <input
             type="password"
             className="form-control password"
             id="cpassword"
             ref={confirmPassword}
+            onChange={checkPasswordMatch}
             placeholder="Confirm Password"
           />
+          <small className="errorMsg">{samePassword}</small>
         </div>
         <div className="form-group txtBox-spacing">
-          <button type="submit" className="bigButton" onClick={signUp}>
+          <button type="submit"
+            className={(passwordLength.length === 0 && samePassword.length === 0 && isValid.length === 0) ? "bigButton" : "bigButton disabled"}
+            onClick={signUp} disabled={(passwordLength.length === 0 && samePassword.length === 0 && isValid.length === 0) ? false : true}>
             Signup
           </button>
         </div>
         <small className="redirectLink">
           Already have an Account ? <Link to="/login">Login</Link>
         </small>
-        {/* </form> */}
-
-      </div>
+      </div> : <h1>You are already login</h1>}
     </div>
   );
 }

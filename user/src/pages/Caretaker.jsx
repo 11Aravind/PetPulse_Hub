ee2827b56575ececc53c234b9efaca9780d8542c
11Aravind <aravindsreenandanam11@gmail.com>
2024-07-08 10:43:07 +0530

    form element edited

--- user/src/pages/Caretaker.jsx
+++ user/src/pages/Caretaker.jsx
@@ -5,21 +5,40 @@ const Caretaker = () => {
         <h3>Petcare Service</h3>
 <p className="aboutPetcare">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia culpa impedit asperiores doloremque, ipsa atque incidunt expedita dicta laudantium pariatur nobis quos adipisci at provident, beatae aliquid optio fugiat perspiciatis?</p>
     <div className="form-container">
-    <form className="row g-3">
-    <div className="col-md-4">
+    <form className="row g-2">
+    <div className="col-md-3">
     <label for="inputState" className="form-label">Pet type</label>
     <select id="inputState" className="form-select">
       <option selected>Choose...</option>
       <option>...</option>
     </select>
   </div>
-  <div className="col-md-2">
+  <div className="col-md-3">
     <label for="inputState" className="form-label">Type</label>
     <input type="text" className="form-control" id="inputPassword4"/>
   </div>
-  <div className="col-md-6">
+  <div className="col-md-3">
     <label for="inputPassword4" className="form-label">Owner name</label>
     <input type="text" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-md-3">
+    <label for="inputPassword4" className="form-label">Owner ID proof</label>
+    <input type="file" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-md-4">
+    <label for="inputPassword4" className="form-label">Phone No</label>
+    <input type="text" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-md-4">
+    <label for="inputPassword4" className="form-label">Phone No2(Optional)</label>
+    <input type="text" className="form-control" id="inputPassword4" placeholder="Alternative  No"/>
+  </div>
+  <div className="col-md-4">
+    <label for="inputPassword4" className="form-label">Select Pet hostel</label>
+    <select id="inputState" className="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
   </div>
   <div className="col-md-3">
     <label for="inputCity" className="form-label">Picup date</label>
@@ -38,13 +57,12 @@ const Caretaker = () => {
     <input type="time" className="form-control" id="inputCity"/>
   </div>
   <div className="col-12">
-    <label for="inputAddress" className="form-label">Address</label>
-    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
+    <label for="inputAddress" className="form-label">Address(Address,locality,city,picode,state)</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>  </div>
 
 
   <div className="col-12">
-    <button type="submit" className="btn btn-primary">Sign in</button>
+    <button type="button" className="bigButton">Set</button>
   </div>
 </form>
     </div>

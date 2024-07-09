1cb357535d7e3ba4522c399cfacd7b4688eb37b3
11Aravind <aravindsreenandanam11@gmail.com>
2024-07-09 10:34:43 +0530

    order component ui was created

--- user/src/pages/Orders.jsx
+++ user/src/pages/Orders.jsx
@@ -36,55 +36,70 @@ const Orders = () => {
 
   return (
     <div className="container">
-      {/* <h1 className="heading">Our orders</h1> */}
       {orders.length === 0 ? (
         <p>No orders found.</p>
       ) : (
-        orders.map((order, index) => {
-          const address = addressArray.find((item) => item._id === order.addressId);
-          return (
-            <div key={index}>
-              {order.items.map((item, key) => (
-               <div className="order-container">
-                 <div className="order-row" key={key}>
-                  <div className="img">
-                    <img src={imgPath + item.image} alt="img" />
-                  </div>
-                  <div className="order-description">
-                    <p>{item.name}</p>
-                    {/* Quantity: {item.quantity}<br />
-                    Payment Mode: {order.paymentMode}, Date: {order.dateOfOrder}<br />
-                    Address: {address ? address.address : 'Address not found'} */}
-                  </div>
-                  <div className="mediumfont">
-                  ₹{item.quantity}
-                  </div>
-                  <div className="cancel-order">
-                    {/* <button className="cancelBtn" id={order._id}>cancel</button> */}
-                   <div className="flag-conatiner"> 
-                   <span className="order-flag"></span>
-                        <span style={{    "fontWeight": "600"}}> Delivered on Apr 09</span>
-                   </div>
-                    <span>Your item has been delivered</span>
-                  </div>
-                </div>
-                  <div className="deliver-address">
-                    <div className="address-left">
-                      <div className="address-headding mediumfont">Delivery Address</div>
-                      <div className="desc">Sreenandanam, Muthupilakkad kizhakku Kunnathur Subdistrict, Kollam District - 690520, Kerala</div>
-                      <div className="head mediumfont">Phone number</div>
-                      <span>8848310248, 8129365304</span>
+       <div className="main">
+        <div className="left-filter">
+          <div className="mediumfont">Filters</div>
+          <ul className="filter-ul">
+            <li><div><input type="checkbox" className="checkbox" />On the way</div></li>
+            <li><div><input type="checkbox" className="checkbox" />Delivered</div></li>
+            <li><div><input type="checkbox" className="checkbox" />Cancelled</div></li>
+            
+          </ul>
+
+        </div>
+        <div className="right-order">
+          {
+             orders.map((order, index) => {
+              const address = addressArray.find((item) => item._id === order.addressId);
+              return (
+                <div key={index}>
+                  {order.items.map((item, key) => (
+                   <div className="order-container">
+                     <div className="order-row" key={key}>
+                      <div className="img">
+                        <img src={imgPath + item.image} alt="img" />
+                      </div>
+                      <div className="order-description">
+                        <p>{item.name}</p>
+                        {/* Quantity: {item.quantity}<br />
+                        Payment Mode: {order.paymentMode}, Date: {order.dateOfOrder}<br />
+                        Address: {address ? address.address : 'Address not found'} */}
+                      </div>
+                      <div className="mediumfont">
+                      ₹{item.quantity}
+                      </div>
+                      <div className="cancel-order">
+                        {/* <button className="cancelBtn" id={order._id}>cancel</button> */}
+                       <div className="flag-conatiner"> 
+                       <span className="order-flag"></span>
+                            <span style={{    "fontWeight": "600"}}> Delivered on Apr 09</span>
+                       </div>
+                        <span>Your item has been delivered</span>
+                      </div>
                     </div>
-                    <div className="address-right">
-                 <div className="address-headding mediumfont">More actions</div>   
-                 <div className="desc">Download Invoice</div>
+                      <div className="deliver-address">
+                        <div className="address-left">
+                          <div className="address-headding mediumfont">Delivery Address</div>
+                          <div className="desc">Sreenandanam, Muthupilakkad kizhakku Kunnathur Subdistrict, Kollam District - 690520, Kerala</div>
+                          <div className="head mediumfont">Phone number</div>
+                          <span>8848310248, 8129365304</span>
+                        </div>
+                        <div className="address-right">
+                     <div className="address-headding mediumfont">More actions</div>   
+                     <div className="desc">Download Invoice</div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          );
-        })
+              );
+            })
+          }
+        </div>
+       </div>
       )}
     </div>
   );
